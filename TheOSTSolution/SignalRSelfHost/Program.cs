5dd81482792b9982905c4cbfc16f0968f818466b
zhaowf <zwf2015@163.com>
2015-06-12 14:23:19 +0800

    聊天服务托管测试成功。

--- TheOSTSolution/SignalRSelfHost/Program.cs
+++ TheOSTSolution/SignalRSelfHost/Program.cs
@@ -4,6 +4,16 @@ using Owin;
 
 namespace SignalRSelfHost
 {
+    /// <summary>
+    /// 地址：http://www.asp.net/signalr/overview/deployment/tutorial-signalr-self-host
+    /// 1.建项目
+    /// 2.安装包
+    ///     	Install-Package Microsoft.AspNet.SignalR.SelfHost --This command adds the SignalR 2 Self-Host libraries to the project.
+    ///         Install-Package Microsoft.Owin.Cors --This command adds the Microsoft.Owin.Cors library to the project. This library will be used for cross-domain support, which is required for applications that host SignalR and a web page client in different domains. Since you'll be hosting the SignalR server and the web client on different ports, this means that cross-domain must be enabled for communication between these components.
+    /// 3.编写Owin.Startup
+    /// 4.编写Hub
+    /// </summary>
+
     class Program
     {
         static void Main(string[] args)
---
+++ TheOSTSolution/TestService/ChatService.cs
@@ -0,0 +1,76 @@
+﻿using System.ServiceProcess;
+using Microsoft.AspNet.SignalR;
+using Microsoft.Owin;
+using Microsoft.Owin.Cors;
+using Microsoft.Owin.Hosting;
+using Owin;
+
+
+[assembly: OwinStartup(typeof(TestService.ChatService.Startup))]
+namespace TestService
+{
+    /// <summary>
+    /// 聊天服务
+    /// 安装 installutil SERVICENAME.exe
+    /// 卸载 installutil /u SERVICENAME.exe
+    /// </summary>
+    public partial class ChatService : ServiceBase
+    {
+        /// <summary>
+        /// 初始化服务。
+        /// </summary>
+        public ChatService()
+        {
+            InitializeComponent();
+            /// 服务的默认名称是 Service1 ，这里重写名称
+            this.ServiceName = "SingalR-Chat-Service";
+        }
+
+        /// <summary>
+        /// 启动服务
+        /// </summary>
+        /// <param name="args"></param>
+        protected override void OnStart(string[] args)
+        {
+            string _url = "http://localhost:65532";
+            WebApp.Start(_url);
+        }
+        /// <summary>
+        /// 停止服务
+        /// </summary>
+        protected override void OnStop()
+        {
+        }
+
+        /// <summary>
+        /// Owin Startup 映射SignalR路由。
+        /// the class containing the configuration for the SignalR server (the only configuration this tutorial uses is the call to UseCors), and the call to MapSignalR, which creates routes for any Hub objects in the project.
+        /// </summary>
+        public class Startup
+        {
+            public void Configuration(IAppBuilder app)
+            {
+                app.UseCors(CorsOptions.AllowAll);
+                app.MapSignalR();
+            }
+        }
+
+        /// <summary>
+        /// MyHub, the SignalR Hub class that the application will provide to clients. 
+        /// This class has a single method: Send, that clients will call to broadcast a message to all other connected clients.
+        /// </summary>
+        public class MyHub : Hub
+        {
+            public void Send(string name, string message)
+            {
+                Clients.All.addMessage(name, message);
+            }
+        }
+
+        ///客户端要为Hub设置指定的URL
+        ///<script src="http://localhost:8097/signalr/hubs"></script>
+        ///$.connection.hub.url = "http://localhost:8097/signalr";
+        ///var chat = $.connection.myHub;
+        ///...
+    }
+}
