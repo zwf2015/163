0a2c14dfecc82368b5cdf200b2a1299498857759
zhaowf <zwf2015@163.com>
2015-07-15 09:26:46 +0800

    T4 测试项目。

---
+++ TheOSTSolution/T4ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace T4ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello,{0}!", "Friday");
+        }
+    }
+}
