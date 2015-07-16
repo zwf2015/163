9b2e8cc31bbdf8a18171ecd0abfff21feb03d210
zhaowf <zwf2015@163.com>
2015-07-16 17:06:57 +0800

    T4测试结束。

---
+++ TheOSTSolution/T4ConsoleApp/WriteLine.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace T4ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			Console.WriteLine("hello,{0}!", "1");
+			Console.WriteLine("hello,{0}!", "2");
+			Console.WriteLine("hello,{0}!", "3");
+			Console.WriteLine("hello,{0}!", "4");
+			Console.WriteLine("hello,{0}!", "5");
+			Console.WriteLine("hello,{0}!", "6");
+			Console.WriteLine("hello,{0}!", "7");
+			
+		}
+    }
+}
