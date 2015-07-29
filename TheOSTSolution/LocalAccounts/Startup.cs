9f3f4461f8a627366314f2f2a4b0560fc193f846
zhaowf <zwf2015@163.com>
2015-07-29 14:18:30 +0800

    T4

--- TheOSTSolution/LocalAccounts/Startup.cs
+++ TheOSTSolution/LocalAccounts/Startup.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using Microsoft.Owin;
+﻿using Microsoft.Owin;
 using Owin;
 
 [assembly: OwinStartup(typeof(LocalAccounts.Startup))]
@@ -12,6 +9,7 @@ namespace LocalAccounts
     {
         public void Configuration(IAppBuilder app)
         {
+            //注册Middleware到OWIN管道
             ConfigureAuth(app);
         }
     }
